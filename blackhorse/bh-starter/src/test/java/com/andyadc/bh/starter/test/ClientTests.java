026ee014afd300b62a678aba10e8f9c60107cffb
andaicheng <andaicheng@gmail.com>
2023-05-30 22:16:17 +0800

    channel

--- blackhorse/bh-starter/src/test/java/com/andyadc/bh/starter/test/ClientTests.java
+++ blackhorse/bh-starter/src/test/java/com/andyadc/bh/starter/test/ClientTests.java
@@ -4,10 +4,12 @@ import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
-import org.junit.jupiter.api.Test;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,12 +20,14 @@ public class ClientTests {
     private static final Logger logger = LoggerFactory.getLogger(ClientTests.class);
 
     public static void main(String[] args) throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup(2);
         Bootstrap bootstrap = new Bootstrap()
-                .group(new NioEventLoopGroup(2))
+                .group(group)
                 .channel(NioSocketChannel.class)
                 .handler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel channel) throws Exception {
+                        channel.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
                         channel.pipeline().addLast(new StringEncoder());
                     }
                 });
@@ -32,25 +36,33 @@ public class ClientTests {
         Channel channel = channelFuture.sync().channel();
         logger.info("{}", channel);
 
-        Thread thread = new Thread(() -> {
+        new Thread(() -> {
             Scanner scanner = new Scanner(System.in);
             while (true) {
                 String line = scanner.nextLine();
                 if ("quit".equalsIgnoreCase(line)) {
-                    channel.closeFuture();
+                    channel.close();
                     logger.info("close");
                     return;
                 }
                 channel.writeAndFlush(line);
             }
-        }, "console-read");
-        thread.setDaemon(true);
-        thread.start();
-    }
+        }, "console-read").start();
 
-    @Test
-    public void testCloseFutureClient() throws Exception {
+        ChannelFuture closeFuture = channel.closeFuture();
+        logger.info("waiting close {}", closeFuture);
+        closeFuture.sync();
+        logger.info("channel closed {}", closeFuture);
 
+        group.shutdownGracefully();
+
+//        closeFuture.addListener(new ChannelFutureListener() {
+//            @Override
+//            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+//                logger.info("{} closed", channelFuture);
+//        group.shutdownGracefully();
+//            }
+//        });
     }
 
 }

70292ceccd25b871dbfac32c547dc0442a1e3a25
andyadc <andaicheng@gmail.com>
2023-06-01 19:42:15 +0800

    packet

--- blackhorse/bh-advanced/src/main/java/com/andyadc/bh/advanced/packet/PacketServer.java
+++ blackhorse/bh-advanced/src/main/java/com/andyadc/bh/advanced/packet/PacketServer.java
@@ -3,11 +3,11 @@ package com.andyadc.bh.advanced.packet;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import org.slf4j.Logger;
@@ -24,10 +24,12 @@ public class PacketServer {
         ServerBootstrap serverBootstrap = new ServerBootstrap()
                 .group(boss, worker)
                 .channel(NioServerSocketChannel.class)
-                .option(ChannelOption.SO_RCVBUF, 10) //
+//                .option(ChannelOption.SO_RCVBUF, 10) // 调整系统的接收缓冲区(滑动窗口)滑动窗口
+//                .childOption(ChannelOption.RCVBUF_ALLOCATOR, new AdaptiveRecvByteBufAllocator(16, 16, 16)) // 调整Netty的接收缓冲区
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel channel) throws Exception {
+                        channel.pipeline().addLast(new FixedLengthFrameDecoder(10));
                         channel.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
                     }
                 });

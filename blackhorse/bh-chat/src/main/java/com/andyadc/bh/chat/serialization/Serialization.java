eb50f73289741a90aaa15cbe2301082a871dc250
andaicheng <andaicheng@gmail.com>
2023-06-06 22:35:45 +0800

    Serialization

--- blackhorse/bh-chat/src/main/java/com/andyadc/bh/chat/serialization/Serialization.java
+++ blackhorse/bh-chat/src/main/java/com/andyadc/bh/chat/serialization/Serialization.java
@@ -5,4 +5,31 @@ public interface Serialization {
     <T> byte[] serialize(T obj);
 
     <T> T deserialize(byte[] data, Class<T> clazz);
+
+    enum Type implements Serialization {
+
+        JDK {
+            @Override
+            public <T> byte[] serialize(T obj) {
+                return new byte[0];
+            }
+
+            @Override
+            public <T> T deserialize(byte[] data, Class<T> clazz) {
+                return null;
+            }
+        },
+
+        JSON {
+            @Override
+            public <T> byte[] serialize(T obj) {
+                return new byte[0];
+            }
+
+            @Override
+            public <T> T deserialize(byte[] data, Class<T> clazz) {
+                return null;
+            }
+        }
+    }
 }
